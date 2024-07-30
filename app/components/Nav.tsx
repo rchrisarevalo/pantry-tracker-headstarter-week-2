2ea928240dd78d542332f08e1fd9c86f83991423
rchrisarevalo <rubenplayer105@gmail.com>
2024-07-30 13:24:59 -0500

    Pushing current changes

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,16 +2,15 @@ import React from "react";
 
 const Nav = () => {
     return (
-        <nav className="grid grid-cols-2 fixed top-0 w-full items-center justify-center bg-slate-400">
+        <nav className="grid grid-cols-2 fixed top-0 w-full items-center justify-center pt-3 pb-3 text-black">
             <ul className="flex flex-row space-x-6">
                 <li className="p-4 pr-5 pl-5">Home</li>
                 <li className="p-4 pr-5 pl-5">About</li>
                 <li className="p-4 pr-5 pl-5">FAQ</li>
             </ul>
             <ul className="flex flex-row justify-end space-x-6">
-                <li className="p-4 pr-5 pl-5">Home</li>
-                <li className="p-4 pr-5 pl-5">About</li>
-                <li className="p-4 pr-5 pl-5">FAQ</li>
+                <li className="p-4 pr-5 pl-5">Sign In</li>
+                <li className="p-4 pr-5 pl-5">Sign Up</li>
             </ul>
         </nav>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import { Button } from "@mui/material";
+import { Button, Card } from "@mui/material";
 import Nav from "./components/Nav";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-white text-black items-center justify-center space-y-5 p-24 font-roboto">
       <Nav />
-      <div className="grid grid-cols-2 items-center justify-center space-x-16 bg-slate-600 text-white rounded-2xl p-5 ml-10 mr-10">
+      <div className="grid grid-cols-2 items-center justify-center space-x-16 text-black rounded-2xl p-5 ml-10 mr-10">
         <div className="flex flex-col justify-center text-left space-y-5">
           <h1 className="text-3xl ml-5 mr-5 text-left font-extrabold">Pantry Tracker</h1>
           <i className="ml-5 mr-5">This app will help you keep track of the number of items you store in your pantry.</i>
         </div>
         <form className="flex flex-col justify-center text-black space-y-5 p-10">
-          <label className="text-white text-left">Enter your item:</label>
-          <input className="p-3 rounded-sm"></input>
-          <label className="text-white text-left">How much are you going to store?</label>
-          <input className="p-3 rounded-sm" type="number"></input>
+          <label className=" text-left">Enter your item:</label>
+          <input className="p-3 rounded-sm border-b-2 border-b-black"></input>
+          <label className="text-left">How much are you going to store?</label>
+          <input className="p-3 rounded-sm border-b-2 border-b-black" type="number"></input>
           <div className="flex flex-row items-center justify-center space-x-10">
             <Button variant="contained" className="p-3 pl-6 pr-6">Add Item</Button>
             <Button variant="contained" className="p-3 pl-6 pr-6">Remove Item</Button>
